980d81299f5eef5a706f6c2a04b0f6ff7c783d6d
simonas <simonas.valkiunas@gmail.com>
2021-04-25 22:01:15 +0300

    removed sre_hm submodule

---
+++ chef-repo/cookbooks/sre_hm/metadata.rb
@@ -0,0 +1,15 @@
+name 'sre_hm'
+maintainer 'Simonas Valkiūnas'
+maintainer_email 'simonas.valkiunas@gmail.com'
+license 'MIT'
+description 'SRE Homework task'
+version '1.0.0'
+chef_version '>= 14'
+
+
+supports 'centos'
+supports 'redhat'
+
+depends 'prometheus_exporters', '>= 0.17.2'
+# depends 'grafana', '~> 8.8.0'
+depends 'prometheus-platform', '~> 2.2.0'
---
+++ chef-repo/cookbooks/sre_hm/recipes/default.rb
@@ -0,0 +1,43 @@
+#
+# Cookbook:: sre_academy
+# Recipe:: default
+#
+# The MIT License (MIT)
+#
+# Copyright:: 2021, The Authors
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
+
+# check if recipe has started, remove later
+
+node.override['prometheus-platform']['components']['prometheus']['install?'] = true
+node.override['prometheus-platform']['components']['prometheus']['install?'] = true
+
+listen_ip = '127.0.0.1'
+
+node_exporter 'main' do
+  web_listen_address "#{listen_ip}:9100"
+  action [:enable, :start]
+end
+
+
+
+include_recipe 'prometheus-platform::default'
+include_recipe 'prometheus_exporters::node'
+
