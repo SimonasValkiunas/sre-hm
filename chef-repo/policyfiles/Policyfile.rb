1edd8de3e50ba8e4630a687fdd993bc4e185a8ad
simonas <simonas.valkiunas@gmail.com>
2021-04-26 19:42:42 +0300

    changed policy file path v3

--- chef-repo/policyfiles/Policyfile.rb
+++ chef-repo/policyfiles/Policyfile.rb
@@ -1,13 +1,13 @@
 name "Policyfile"
 
 default_source :supermarket, "https://supermarket.chef.io" do |s|
-    s.preferred_for [ "prometheus_exporters", "prometheus-platform" ]
+    s.preferred_for [ "prometheus_exporters", "prometheus-platform", "grafana"]
   end
 
 cookbook 'prometheus_exporters', '~> 0.17.2', :supermarket
 cookbook 'prometheus-platform', '~> 2.2.0', :supermarket
 cookbook 'grafana', '~> 8.8.0', :supermarket
-cookbook "sre_hm", path: "./../cookbooks/sre_hm"
+cookbook "sre_hm", path: "../cookbooks/sre_hm"
 
 run_list "sre_hm"
 
