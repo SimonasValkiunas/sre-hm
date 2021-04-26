50aeca53ad2148f181dc031446c3b10e11dde568
simonas <simonas.valkiunas@gmail.com>
2021-04-26 16:04:30 +0300

    added recording rules, minor fix v2

--- chef-repo/cookbooks/sre_hm/recipes/default.rb
+++ chef-repo/cookbooks/sre_hm/recipes/default.rb
@@ -37,22 +37,86 @@ node_exporter 'main' do
 end
 
 # configure recording rules
-node.override[cookbook_name]['components']['prometheus']['rules'] = {
-  'alerting' => [],
-  'recording' => [
-    "instance:node_num_cpu:sum",
-    "instance:node_cpu_utilisation:rate1m",
-    "instance:node_load1_per_cpu:ratio",
-    "instance:node_memory_utilisation:ratio",
-    "instance:node_vmstat_pgmajfault:rate1m",
-    "instance_device:node_disk_io_time_seconds:rate1m",
-    "instance_device:node_disk_io_time_weighted_seconds:rate1m",
-    "instance:node_network_receive_bytes_excluding_lo:rate1m",
-    "instance:node_network_transmit_bytes_excluding_lo:rate1m",
-    "instance:node_network_receive_drop_excluding_lo:rate1m",
-    "instance:node_network_transmit_drop_excluding_lo:rate1m"
-  ]
-}
+# node.override[cookbook_name]['components']['prometheus']['rules'] = {
+#   'alerting' => [],
+#   'recording' => [
+#     "instance:node_num_cpu:sum",
+#     "instance:node_cpu_utilisation:rate1m",
+#     "instance:node_load1_per_cpu:ratio",
+#     "instance:node_memory_utilisation:ratio",
+#     "instance:node_vmstat_pgmajfault:rate1m",
+#     "instance_device:node_disk_io_time_seconds:rate1m",
+#     "instance_device:node_disk_io_time_weighted_seconds:rate1m",
+#     "instance:node_network_receive_bytes_excluding_lo:rate1m",
+#     "instance:node_network_transmit_bytes_excluding_lo:rate1m",
+#     "instance:node_network_receive_drop_excluding_lo:rate1m",
+#     "instance:node_network_transmit_drop_excluding_lo:rate1m"
+#   ]
+# }
+
+file '/opt/prometheus-2.25.2/rules/node_exporter_recording_rules' do
+  content '"groups":
+  - "name": "node-exporter.rules"
+    "rules":
+    - "expr": |
+        count without (cpu) (
+          count without (mode) (
+            node_cpu_seconds_total{job="node"}
+          )
+        )
+      "record": "instance:node_num_cpu:sum"
+    - "expr": |
+        1 - avg without (cpu, mode) (
+          rate(node_cpu_seconds_total{job="node", mode="idle"}[1m])
+        )
+      "record": "instance:node_cpu_utilisation:rate1m"
+    - "expr": |
+        (
+          node_load1{job="node"}
+        /
+          instance:node_num_cpu:sum{job="node"}
+        )
+      "record": "instance:node_load1_per_cpu:ratio"
+    - "expr": |
+        1 - (
+          node_memory_MemAvailable_bytes{job="node"}
+        /
+          node_memory_MemTotal_bytes{job="node"}
+        )
+      "record": "instance:node_memory_utilisation:ratio"
+    - "expr": |
+        rate(node_vmstat_pgmajfault{job="node"}[1m])
+      "record": "instance:node_vmstat_pgmajfault:rate1m"
+    - "expr": |
+        rate(node_disk_io_time_seconds_total{job="node", device!=""}[1m])
+      "record": "instance_device:node_disk_io_time_seconds:rate1m"
+    - "expr": |
+        rate(node_disk_io_time_weighted_seconds_total{job="node", device!=""}[1m])
+      "record": "instance_device:node_disk_io_time_weighted_seconds:rate1m"
+    - "expr": |
+        sum without (device) (
+          rate(node_network_receive_bytes_total{job="node", device!="lo"}[1m])
+        )
+      "record": "instance:node_network_receive_bytes_excluding_lo:rate1m"
+    - "expr": |
+        sum without (device) (
+          rate(node_network_transmit_bytes_total{job="node", device!="lo"}[1m])
+        )
+      "record": "instance:node_network_transmit_bytes_excluding_lo:rate1m"
+    - "expr": |
+        sum without (device) (
+          rate(node_network_receive_drop_total{job="node", device!="lo"}[1m])
+        )
+      "record": "instance:node_network_receive_drop_excluding_lo:rate1m"
+    - "expr": |
+        sum without (device) (
+          rate(node_network_transmit_drop_total{job="node", device!="lo"}[1m])
+        )
+      "record": "instance:node_network_transmit_drop_excluding_lo:rate1m"'
+  mode '0755'
+  owner 'prometheus'
+  group 'prometheus'
+end
 
 # configures prometheus to scrape data from node exporter
 node.override['prometheus-platform']['components']['prometheus']['config']['scrape_configs'] = {
